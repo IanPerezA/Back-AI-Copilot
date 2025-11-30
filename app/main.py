07a31b7b27ee4f5210bd5de556260df61f716349
IanPerezDev <ian.perez@noktos.com>
2025-11-30 13:44:13 -0600

    fix pequeño error en el link de front de produccion

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://front-ai-copilot.vercel.app/"
+    "https://front-ai-copilot.vercel.app"
 ]
 
 app.add_middleware(
